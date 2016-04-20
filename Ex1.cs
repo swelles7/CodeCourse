403d94e7d95ded3db05cb0f8a6d76983c09c4ae0
swelles7 <swelles7@gmail.com>
2016-04-20 16:50:10 -0400

    Initial Commit

---
+++ Ex1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            string answer;
+            bool keepLooping = true;
+
+            while(keepLooping)
+            {
+                Console.WriteLine("Please enter a number");
+                answer = Console.ReadLine();
+                if (int.TryParse(answer, out number))
+                {
+                    //number = Convert.ToInt16(answer);
+                    Console.WriteLine("Thanks");
+                    keepLooping = false;
+                }
+                   
+            }
+        }
+    }
+}
